8e0d2620778844eec324c8755075583e9b6ae1bd
Christian Emmanuel Davis Orquiz <davisoc26@gmail.com>
2021-10-04 19:51:21 -0500

    Created Controllers

---
+++ HotelListing/Controllers/CountryController.cs
@@ -0,0 +1,25 @@
+﻿using HotelListing.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger _logger;
+
+        public CountryController(IUnitOfWork unitOfWork, ILogger logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+    }
+}
